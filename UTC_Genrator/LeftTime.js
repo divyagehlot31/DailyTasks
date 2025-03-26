4f80a9b97eeeafd4431d39b2dda2dd1b904819d8
divyagehlot31 <divyagehlot3112@gmail.com>
2025-03-26 13:18:59 +0530

    navbar and slider complete updated

--- UTC_Genrator/LeftTime.js
+++ UTC_Genrator/LeftTime.js
@@ -5,17 +5,20 @@ function timeLeft(eventTime) {
     //local time
     let localtime = new Date();
 
+    let a = event_time.toLocaleString("en-US", { timeZone: "UTC" });
+    console.log(a);
+
     // time difference
     let timeDiff = event_time - localtime;
-
     
     if (timeDiff <= 0) {
         console.log("LIVE");
-        return;
+        return; 
+
     }
 
     // Convert milliseconds into days ,hours , minute ,seconds
-    let days = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+    let days = Math.floor(timeDiff / (1000 * 60 * 60 * 24)); 
     let hours = Math.floor((timeDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     let minutes = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
     let seconds = Math.floor((timeDiff % (1000 * 60)) / 1000);
@@ -25,7 +28,7 @@ function timeLeft(eventTime) {
 }
 
 
-setTimeout(() => {
-    timeLeft("2025-03-25T05:27:43Z"); //UTC time
-}, 0);
+setInterval(() => {
+    timeLeft("2025-03-27T05:27:43Z"); //UTC time
+}, 1000);
 
