d6d78fe6becd15e6ff67dd16166312dc24dad236
divyagehlot31 <divyagehlot3112@gmail.com>
2025-04-08 19:00:23 +0530

    Practical demo of Component's lifecycle

---
+++ React/react-with-vite/components/Fun.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+//////////////////Example of functional components
+
+
+// function Fun() {
+//   return (
+//     <div>
+//         <h1>hy! this is function components!!!!!!!!!!</h1>
+      
+//     </div>
+//   )
+// }
+
+// const Fun = () =>{
+//     return(
+//         <div>
+//             <h1>hyy! this is function components!!!!!!!!!!</h1>
+//         </div>
+//     )
+// }
+
+export default Fun;
---
+++ React/react-with-vite/components/Person.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import Fun from "./fun";
+
+/////////////example of class components
+
+// class Person extends Component {
+//   //   constructor() {}
+
+//   render() {
+//     return (
+//       <div>
+//         <div>
+//           Name:Divya
+//         </div>
+
+//         <div>
+//           Name:Krupa
+//         </div>
+
+// <Fun/>
+//       </div>
+//     );
+//   }
+// }
+
+export default Person;
